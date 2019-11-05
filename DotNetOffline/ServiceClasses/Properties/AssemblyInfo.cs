1f33410b28437a4c9ee751d42fc2ae45f7a350c8
cyberdragon42 <panchyshynmariait@gmail.com>
2019-11-05 14:21:29 +0200

    removed unnessesary directories, renamed folders

--- DotNetOffline/ServiceClasses/Properties/AssemblyInfo.cs
+++ DotNetOffline/ServiceClasses/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ServiceClasses")]
+[assembly: AssemblyTitle("TrainingFour")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ServiceClasses")]
+[assembly: AssemblyProduct("TrainingFour")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("7ad60b77-0883-4351-8159-a020d8aec24c")]
+[assembly: Guid("bff686a6-8086-4091-adbd-fb6fe87ae2d5")]
 
 // Version information for an assembly consists of the following four values:
 //
